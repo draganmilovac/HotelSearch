fea7dc28a68a25ae73ff713108e22dafc9c0eb10
AzureAD\DraganMilovac <dragan.milovac@unifiedpost.com>
2024-09-13 10:37:33 +0200

    Add UnitTest project

---
+++ src/Tests/HotelBooking.Test/Lists/HotelLists.cs
@@ -0,0 +1,64 @@
+﻿using Hotel.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Test.Lists
+{
+    public class HotelLists
+    {
+        #region Constructors
+        public HotelLists()
+        {
+            Hotels = new Hotels();
+            PopulateList();
+        }
+        #endregion
+
+        #region Properties
+
+        public Hotels Hotels { get; set; }
+        #endregion
+
+        #region Private methods
+
+        private void PopulateList()
+        {
+            Hotels.Add(new HotelData.Models.Hotel()
+            {
+                Id = 1,
+                Name = "Hotel 1",
+                Price = 20,
+                Latitude = 45.254374284753254,
+                Longitude = 19.821862950036017
+            });
+            Hotels.Add(new HotelData.Models.Hotel()
+            {
+                Id = 2,
+                Name = "Hotel 2",
+                Price = 20,
+                Latitude = 45.2211327840916,
+                Longitude = 19.848985445749207
+            });
+            Hotels.Add(new HotelData.Models.Hotel()
+            {
+                Id = 3,
+                Name = "Hotel 3",
+                Price = 20,
+                Latitude = 45.237212010810886,
+                Longitude = 19.810189977197428
+            });
+            Hotels.Add(new HotelData.Models.Hotel()
+            {
+                Id = 4,
+                Name = "Hotel 4",
+                Price = 20,
+                Latitude = 45.25689124664803,
+                Longitude = 19.814676541895956
+            });
+        }
+        #endregion
+    }
+}
---
+++ src/Tests/HotelBooking.Test/Repositories/HotelRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using Hotel.Infrastructure.Repositories;
+using HotelBooking.Test.Lists;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Test.Repositories
+{
+    public class HotelRepositoryTests
+    {
+        #region Private fields
+        private readonly Mock<ILogger<HotelRepository>> _mockLogger;
+        private readonly HotelLists _hotelLists;
+        #endregion
+
+        #region Constructors
+        public HotelRepositoryTests()
+        {
+            _mockLogger = new Mock<ILogger<HotelRepository>>();
+            _hotelLists = new HotelLists();
+        }
+        #endregion
+
+        #region Test methods
+        [Fact]
+        public async Task CreateHotelAsync_ShouldPopulateListAndReturnId()
+        {
+            HotelData.Models.Hotel hotel = new HotelData.Models.Hotel()
+            {
+                Name = "Hotel 5",
+                Price = 60,
+                Latitude = 46.257907355517624,
+                Longitude = 20.81236695106882
+            };
+            var hotelRepository = new HotelRepository(_hotelLists.Hotels, _mockLogger.Object);
+            var result = await hotelRepository.CreateAsync(hotel);
+            Assert.Equal(5, result);
+        }
+        #endregion
+    }
+}
